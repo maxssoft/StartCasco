b70d2095811d96ad5dee4cfd4e72dbb636920ad1
Max Sidorov <maxssoft@yandex.ru>
2018-05-03 22:32:15 +0300

    add splash screen

--- app/src/main/java/ru/telematica/casco2go/ui/activity/SplashActivity.kt
+++ app/src/main/java/ru/telematica/casco2go/ui/activity/SplashActivity.kt
@@ -1,14 +1,20 @@
 package ru.telematica.casco2go.ui.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 import ru.telematica.casco2go.R
 
 class SplashActivity : AppCompatActivity() {
 
+    private val handler: Handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        handler.postDelayed(Runnable { startActivity(Intent(this, MainActivity::class.java)) }, 1000)
     }
+
 }
