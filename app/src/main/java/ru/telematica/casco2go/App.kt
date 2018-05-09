1da6f2d58c1bc7ba121a65ec0874d7600e9c3f64
Max Sidorov <maxssoft@yandex.ru>
2018-05-09 12:08:03 +0300

    add Crashlytics

--- app/src/main/java/ru/telematica/casco2go/App.kt
+++ app/src/main/java/ru/telematica/casco2go/App.kt
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatDelegate
 import ru.telematica.casco2go.repository.ConfigRepository
 import ru.telematica.casco2go.service.http.HttpService
 import ru.telematica.casco2go.service.http.RetrofitProvider
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+import com.crashlytics.android.core.CrashlyticsCore
+
+
+
+
 
 /**
  * Created by m.sidorov on 29.04.2018.
@@ -28,7 +35,16 @@ class App : android.support.multidex.MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+
+        initCrashlytics()
         initInstance()
+    }
+
+    private fun initCrashlytics() {
+        val builder = CrashlyticsCore.Builder()
+        //builder.disabled(BuildConfig.DEBUG)
+        val core = builder.build()
+        Fabric.with(this, Crashlytics.Builder().core(core).build())
     }
 
     private fun initInstance() {
